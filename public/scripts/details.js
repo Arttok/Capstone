f74f2a8da22dc9b0f7274ab86e6a706071ee4fd8
Michael Flickinger <Mike.A.Flickinger@gmail.com>
2019-08-30 13:12:41 -0400

    saving work. Dynamic population of team informaiton and changing information.

--- public/scripts/details.js
+++ public/scripts/details.js
@@ -17,15 +17,59 @@ $(function()
     // parameter named objs
     // take a few minutes to examine the attached .json file
     objs = teams;
-
-    $("#register").html('Save');
-      createMngrTable(objs);
-      showPlayers(objs);
-      $("#register").attr("href", "#");
+    showPlayers(objs);
+    createMngrTable(objs);
       //"courses.html?" + "instr=" + $.urlParam('instr') );
   })
+
+  $.getJSON("/api/leagues", function(leagues) 
+    {
+      // the returned data is available in an "already parsed"
+      // parameter named data
+      // take a few minutes to examine the attached .json file
+      leaguesSelect = leagues
+      let x;
+      $("#leaguecode").empty();
+      for (var i = 0; i < leaguesSelect.length; i++) 
+      {
+          $("#leaguecode").append(
+          "<option value=" +
+              leaguesSelect[i].Code +
+              ">" +
+              leaguesSelect[i].Name +
+              "</option>"
+          );
+          if ($("#leaguecode option:selected").val() ==leaguesSelect[i].Code)
+          { //gets the starting max team members and region based upon league.
+            $("#maxteammembers").html(leaguesSelect[i].MaxTeamMembers);
+            $("#region").text(leaguesSelect[i].Region);
+          }
+      }
+
+      createLeagueTable(leaguesSelect);
+    })
 })
 
+
+function createMngrTable(objs)
+{
+  $("#teamname").val(objs.TeamName);
+  $("#managername").val(objs.ManagerName);
+  $("#managerphone").val(objs.ManagerPhone);
+  $("#manageremail").val(objs.ManagerEmail);
+  $("#minmemberage").val(objs.MinMemberAge);
+  $("#maxmemberage").val(objs.MaxMemberAge);
+  $("#teamgender").val(objs.TeamGender);
+  $("#region").val(objs.Region);
+}
+
+function createLeagueTable(leaguesSelect)
+{
+  
+  //console.log($("#leaguecode option:selected").val())
+  //$("#maxteammembers").val(leaguesSelect.MaxTeamMembers);
+  //$("#region").val(leaguesSelect.Region);
+}
 /*This function shows the student table informaiton..
  *
  *
@@ -35,10 +79,23 @@ $(function()
  *@param ---Email--- is the student email.
  */
 function showPlayers(objs) {
+  let tableHead = ["Tag Name", "Email", "Age", "Gender", "Phone", "Region"];
+
+    $("#players").empty();
+    let thead = $("<thead class= text-light>");
+    $("#players").append(thead);
+    let trow = $("<tr>");
+    $("#players thead").append(trow);
+    for (let i = 0; i < tableHead.length; i++) 
+    {
+      let col = $("<th>" + tableHead[i] + "</th>");
+      $("#players thead tr").append(col);
+    }
+
   if (objs.Members.length == 0) {
-    $("#players").html("<table> <tr><td>No Players have Registered</td></td> </table>");
+    $("#players").html("<table class=text-light> <tr><td>No Players have Registered</td></td> </table>");
   } else {
-    let tBody = $("<tbody>");
+    let tBody = $("<tbody class= text-light>");
     $("#players").append(tBody);
     for (let i = 0; i < objs.Members.length; i++) {
       let markup =
@@ -48,11 +105,11 @@ function showPlayers(objs) {
         objs.Members[i].Email +
         "</td><td class=age>" +
         objs.Members[i].Age +
-        "</td><td class=teamgender" +
+        "</td><td class=gender>" +
         objs.Members[i].Gender +
-        "</td><td class=phone" +
+        "</td><td class=phone>" +
         objs.Members[i].Phone +
-        "</td><td class=region" +
+        "</td><td class=region>" +
         objs.Members[i].Region
         "</td><td class='detailbtn'>" +
         '<input type="button" id=' + [i] + ' class="delete" value="Unregister"/>' +
