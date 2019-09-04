1605456dd7c477b46b9ee9c2828cadfa9021968f
Michael Flickinger <Mike.A.Flickinger@gmail.com>
2019-09-04 14:31:14 -0400

    fixed link issue

--- public/scripts/player.js
+++ public/scripts/player.js
@@ -67,7 +67,7 @@ $(function()
             }
         })
 
-        $("#update").click(function() 
+        $("#cancel").click(function() 
         {
             document.location.href = "teamsearch.html";
         })
